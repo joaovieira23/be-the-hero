5e77bd13d3b6dfba6b52bd5fe9fe08e5e83d982f
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-03-31 13:13:33 -0300

    feat: handle incident delete

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -23,6 +23,19 @@ export default function Profile() {
     })
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+      
+      setIncidents(incidents.filter(incident => incident.id !== id))
+    } catch (err) {
+      alert('Erro ao deletar caso, tente novamente.');
+    }
+  }
   return (
     <div className="profile-container">
       <header>
@@ -49,7 +62,7 @@ export default function Profile() {
           <strong>VALOR:</strong>
           <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)} </p>
 
-          <button type="button">
+          <button onClick={() => handleDeleteIncident(incident.id)} type="button">
             <FiTrash2 size={20} color="#A8A8B3" />
           </button>
         </li>
