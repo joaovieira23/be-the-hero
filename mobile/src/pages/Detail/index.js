9dc68cbe55a9555cc307234ae065aa9e239874b5
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-04 15:18:56 -0300

    feat: add expo-mail-composer

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,9 +10,22 @@ import styles from './styles';
 
 export default function Detail() {
   const navigation = useNavigation();
+  const message = 'Olá ASMID, estou entrando em contato pois gostaria de ajudar no caso "Cachorro Atropealdo" com o valor de R$120,00'
 
   function navigateBack() {
     navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['joaovictorvieira.23@hotmail.com'],
+      body: message,
+    })
+  }
+
+  function sendWhatsApp() {
+
   }
 
   return (
@@ -45,7 +59,7 @@ export default function Detail() {
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
